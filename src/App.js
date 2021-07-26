90dd7c8dd4bf1c1e70e329253c8badbb5c8b0ec5
easella <74786054+easella@users.noreply.github.com>
2021-07-26 10:55:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -164,7 +164,7 @@ function App() {
         <button
             onClick={signInWithGithub}
             className="rounded shadow-button pl-6 pr-8 py-3 bg-white hover:bg-gray-50 text-gray-600 font-medium flex items-center justify-center overflow-y-hidden focus:outline-none focus:ring focus:ring-primary-500 focus:ring-opacity-75"><img src="https://github.githubassets.com/favicons/favicon.png" alt="github logo">Sign in With Github</button>
-          >
+          
   
   </div>
       </div>
